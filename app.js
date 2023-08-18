8846ad77ee253e83d30512c8736a78ec8824a9ac
Strasseneck <121754696+Strasseneck@users.noreply.github.com>
2023-08-18 14:41:06 +0200

    Add new structuring via two seperate functions, fix error in computerSelection assingnment

--- app.js
+++ app.js
@@ -1,26 +1,58 @@
  // Player buttons
  const buttons = document.querySelectorAll('.btn');
 
- // Scores object, result variable
- let scores = {Player: 0, Computer: 0}
- let result;
+// Add event listener, pass values, call play round function
+buttons.forEach(button => button.addEventListener('click', () => {game(button)}))
 
- // Get computer selection
- const computerSelection = () => {
+ // Get computer selection function
+function getComputerSelection() {
     const choices = ['rock', 'paper', 'scissors'];
     let randomNumber = Math.floor(Math.random() * choices.length);
     return choices[randomNumber];
  }
 
- // Add event listener, pass values, call play round function
- buttons.forEach(button => button.addEventListener('click', () => {playRound(button.id, computerSelection)}))
+ // Game function
+ function game(button){
+    
+    // Get selections
+    const playerSelection = button.id;
+    const computerSelection = getComputerSelection();
+    
+    // Scores object call round
+    let scores = {player: 0, computer: 0}
+    let finalResult;
+
+    // Play a round
+    let round = playRound(playerSelection, computerSelection);
+    
+    // Record scores
+    if (round.includes("You win!")) {
+        scores.player ++;
+    } else if (round.includes("You lose!")) {
+        scores.computer ++;
+    } else if (round === "It's a draw!") {
+    }
+
+    // Check for end of game and final winner
+    if(scores.player >= 5) {
+        finalResult = 'You win!'
+        return finalWinner
+    } else if (scores.computer >= 5) {
+        finalResult = 'You lose!'
+        return finalWinner
+    }
+ }
 
  // Play round function
  function playRound (playerSelection, computerSelection) {
+
+    // Check for draw
     if (playerSelection === computerSelection) {
         result = 'It\'s a draw';
         return result;
     } else {
+
+        // Check all other cases
         switch (playerSelection) {
             case 'rock':
             if (computerSelection === 'paper') {
@@ -46,9 +78,9 @@
                     result = 'You win! Scissors beat paper!';
                     return result;
                 }
+            }
         }
     }
- }
 
 
 
